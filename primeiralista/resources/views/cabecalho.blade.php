40839cbb2e8be7db73e246d7b1d30398c01176d5
vinithales <127968652+vinithales@users.noreply.github.com>
2025-02-12 20:40:35 -0300

    first commit

---
+++ primeiralista/resources/views/cabecalho.blade.php
@@ -0,0 +1,3 @@
+<ul>
+    <li><a href="/exer1">Exercício 1</a></li>
+</ul>
---
+++ primeiralista/routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/bem-vindo', function(){
+    return  "Seja bem vindo";
+});
+
+
+Route::get("/exer1", function(){
+    return view("exer1");
+});
+
+Route::post('/exer1resp', function(Request $request){
+    $valor1 = intval($request->input('valor1'));
+    $valor2 = intval($request->input('valor2'));
+    
+    $soma = $valor1 + $valor2;
+
+    return view('exer1', compact('soma'));
+});
+
